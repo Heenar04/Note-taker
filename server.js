129f90a441d4e59879186eb2d8ffa519215b735b
hrajpal <hrajpal@ebrevia.com>
2020-12-09 16:04:05 -0500

    updated note taker

--- server.js
+++ server.js
@@ -7,50 +7,32 @@ const fs = require('fs');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //call routefile
 const htRoute = require('./routes/htmlroute');
+const { json } = require("express");
 app.use(express.static("public"));
 app.use('/', htRoute);
 
 app.get('/api/notes', (req,res) => {
-  
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(`err at the database ${err}`);
+        }
+        res.json(JSON.parse(data))
+    })
 });
 //create new Note
 app.post('/api/notes', (req, res) => {
-
-  let noteNew = req.body;
-
-      fs.readFile('db/db.json', 'utf8', (err, data) => {
-          if (err) {
-              console.log(`err at the database ${err}`);
-          } 
-          
-          else if (data.length > 2) {
-              obj = JSON.parse(data);
-              obj.push(noteNew);
-
-              fs.writeFile('db/db.json', JSON.stringify(obj), 'utf8', (err) => {
-                  if(err) {
-                      throw err;
-                  }
-                  console.log('Note saved.')
-              });
-         }
-          else {
-              obj = [];
-              obj.push(noteNew);
-              fs.writeFile('db/db.json', JSON.stringify(obj), 'utf8', (err) => {
-                  if(err) {
-                      throw err;
-                  }
-                  console.log('Note saved.')
-              });
-          }
-      });
+let currentNotes = fs.readFileSync("db/db.json",'utf8')
+currentNotes = JSON.parse(currentNotes)
+ req.body.id = currentNotes.length;
+ currentNotes.push(req.body)
+ fs.writeFileSync("db/db.json", JSON.stringify(currentNotes))
+ res.json(JSON.parse(currentNotes))
 });
 
 //delete Note
@@ -84,7 +66,7 @@ app.get('*', (req,res) => {
 
 
 
-
+// create an empty array to delet the data which has no id from the database 
 
 
 
